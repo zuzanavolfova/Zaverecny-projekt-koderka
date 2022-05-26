1365920970b0420cc58548e7cf099ccdf8b63e6f
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-05-26 19:19:37 +0200

    edit style glide__arrows

--- _site/javaScript/menu.js
+++ _site/javaScript/menu.js
@@ -1,9 +1,13 @@
 function menu (){
     let menu=document.querySelector('.menu-mobile__list')
+    let pokus=document.querySelector('.glide__arrow')
     if (menu.style.display === 'none')
         {
         menu.style.display='block';
+        pokus.style.zIndex='-1';
         } else {
             menu.style.display='none';
+            pokus.style.zIndex='1';
+
         }
 }
